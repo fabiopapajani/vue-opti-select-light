c320b7d154efb0fcce7efe1aabd6c89efe58ed4a
Fabio <fabio@paperclicks.net>
2020-06-05 11:11:30 +0200

    Working with structure

--- src/components/OptiSelectLight.vue
+++ src/components/OptiSelectLight.vue
@@ -1,9 +1,34 @@
 <template>
-  <h1>OptiSelectLight</h1>
+  <div class="opti-select-light">
+    <b-dd :lazy="lazy">
+      <template v-slot:button-content>
+        Custom <strong>Content</strong> with <em>HTML</em> via Slot
+      </template>
+      <b-dd-item href="#">An item</b-dd-item>
+      <b-dd-item href="#">Another item</b-dd-item>
+    </b-dd>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'opti-select-light'
+  name: 'vue-opti-select-light',
+  props: {
+    value: null,
+    options: { type: Array, default: () => [] },
+    uniqueKey: { type: [String, Function], default: 'value' },
+    contentKey: { type: [String, Function], default: 'content' },
+    error: { type: Boolean, default: false },
+    searchable: { type: Boolean, default: false },
+    searchField: { type: String, default: 'content' },
+    searchPlaceholder: { type: String, default: 'Search...' },
+    lazy: { type: Boolean, default: false }
+  }
 }
 </script>
+
+<style lang="scss">
+.opti-select-light {
+
+}
+</style>
